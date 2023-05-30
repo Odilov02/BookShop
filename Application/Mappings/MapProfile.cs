f010e63888364bcbb93d119b5efd8d0626181904
diyorbek <diyorbek02odilov@gmail.com>
2023-05-30 08:42:08 +0500

    added Controller

--- Application/Mappings/MapProfile.cs
+++ Application/Mappings/MapProfile.cs
@@ -9,6 +9,8 @@ using Application.DTOs.Users;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Entities.IdentityEntities;
+using System.Collections.Immutable;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Application.Mappings;
 
@@ -30,52 +32,69 @@ public class MapProfile : Profile
         UserMapping();
 
         PermissionMapping();
-
-        void AuthorMapping()
-        {
-            CreateMap<AuthorCreateDTO, Author>().ReverseMap();
-            CreateMap<AuthorUpdateDTO, Author>().ReverseMap();
-            CreateMap<AuthorGetDTO, Author>().ReverseMap();
-        }
-
-        void BookMapping()
-        {
-            CreateMap<Book, Book>().ReverseMap();
-            CreateMap<BookUpdateDTO, Book>().ReverseMap();
-            CreateMap<BookGetDTO, Book>().ReverseMap();
-        }
-
-        void CommentaryMappping()
-        {
-            CreateMap<CommentaryCreateDTO, Commentary>().ReverseMap();
-            CreateMap<CommentaryUpdateDTO, Commentary>().ReverseMap();
-            CreateMap<CommentaryGetDTO, Commentary>().ReverseMap();
-        }
-
-        void RoleMapping()
-        {
-            CreateMap<RoleCreateDTO, Role>().ReverseMap();
-            CreateMap<RoleUpdateDTO, Role>().ReverseMap();
-            CreateMap<RoleGetDTO, Role>().ReverseMap();
-        }
-
-        void CategoryMapping()
-        {
-            CreateMap<CategoryCreateDTO, Category>().ReverseMap();
-            CreateMap<CategoryUpdateDTO, Category>().ReverseMap();
-            CreateMap<CategoryGetDTO, Category>().ReverseMap();
-        }
-
-        void UserMapping()
-        {
-            CreateMap<UserCreateDTO, User>().ReverseMap();
-            CreateMap<UserUpdateDTO, User>().ReverseMap();
-            CreateMap<UserGetDTO, User>().ReverseMap();
-        }
-
-        void PermissionMapping()
-        {
-            CreateMap<PermissionGetDTO, Permission>().ReverseMap();
-        }
     }
+    void AuthorMapping()
+    {
+        CreateMap<AuthorCreateDTO, Author>().ReverseMap();
+        CreateMap<AuthorUpdateDTO, Author>().ReverseMap();
+        CreateMap<Author, AuthorGetDTO>().ForMember(dest => dest.BooksId, opt => opt.MapFrom(src=>src!.Books!.Select(x=>x.Id)));
+        CreateMap<AuthorGetDTO, Author>().ForMember(dest =>dest.Books!.Select(x => x.Id), opt => opt.MapFrom(src=>src.BooksId));
+    }
+
+    void BookMapping()
+    {
+        CreateMap<BookCreateDTO, Book>().ReverseMap();
+        CreateMap<BookUpdateDTO, Book>().ReverseMap();
+        CreateMap<BookGetDTO, Book>().ForMember(dest => dest.Commentaries!.Select(c => c.Id), opt => opt.MapFrom(c => c.CommentariesId));
+        CreateMap<Book, BookGetDTO>().ForMember(dest => dest.CommentariesId!, opt => opt.MapFrom(c => c.Commentaries!.Select(c => c.Id)));
+    }
+
+    void CommentaryMappping()
+    {
+        CreateMap<CommentaryCreateDTO, Commentary>().ReverseMap();
+        CreateMap<CommentaryUpdateDTO, Commentary>().ReverseMap();
+        CreateMap<CommentaryGetDTO, Commentary>().ReverseMap();
+    }
+
+    void RoleMapping()
+    {
+        CreateMap<RoleCreateDTO, Role>().ForMember(dest => dest.permissions!.Select(c => c.Id), opt => opt.MapFrom(c => c.permissionsId));
+        CreateMap<Role, RoleCreateDTO>().ForMember(dest => dest.permissionsId, opt => opt.MapFrom(c => c.permissions!.Select(x=>x.Id)));
+
+        CreateMap<RoleUpdateDTO, Role>().ForMember(dest => dest.permissions!.Select(x => x.Id), opt => opt.MapFrom(c => c.permissionsId));
+        CreateMap<Role, RoleUpdateDTO>().ForMember(dest => dest.permissionsId, opt => opt.MapFrom(c => c.permissions!.Select(x => x.Id)));
+
+        CreateMap<RoleGetDTO, Role>().ForMember(dest => dest.permissions!.Select(x => x.Id), opt => opt.MapFrom(c => c.permissionsId));
+        CreateMap<Role, RoleGetDTO>().ForMember(dest => dest.permissionsId, opt => opt.MapFrom(c => c.permissions!.Select(x => x.Id)));
+    }
+
+
+    void CategoryMapping()
+    {
+        CreateMap<CategoryCreateDTO, Category>().ReverseMap();
+        CreateMap<CategoryUpdateDTO, Category>().ReverseMap();
+        CreateMap<CategoryGetDTO, Category>().ForMember(dest => dest.Books!.Select(x => x.Id), opt => opt.MapFrom(x => x.BooksId));
+        CreateMap<Category, CategoryGetDTO>().ForMember(dest => dest.BooksId, opt => opt.MapFrom(c=>c.Books!.Select(x => x.Id)));
+    }
+
+    void UserMapping()
+    {
+        CreateMap<UserCreateDTO, User>().ReverseMap();
+
+        CreateMap<UserUpdateDTO, User>().ForMember(dest => dest.Roles!.Select(x => x.Id), opt => opt.MapFrom(x => x.RolesId));
+        CreateMap<User, UserUpdateDTO>().ForMember(dest => dest.RolesId, opt => opt.MapFrom(x=>x.Roles!.Select(x => x.Id)));
+        
+        CreateMap<UserGetDTO, User>().ForMember(dest => dest.Roles!.Select(x => x.Id), opt => opt.MapFrom(x => x.RolesId));
+        CreateMap<User, UserGetDTO>().ForMember(dest => dest.RolesId, opt => opt.MapFrom(x=>x.Roles!.Select(x => x.Id)));
+    }
+
+    void PermissionMapping()
+    {
+        CreateMap<PermissionGetDTO, Permission>().ReverseMap();
+    }
+
 }
+
+
+
+
--- WebUI/Controllers/AuthorController.cs
+++ WebUI/Controllers/AuthorController.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.DTOs.Books;
+using Application.DTOs.users;
+using Application.DTOs.Users;
+using Application.Interfaces.ServiceInterfaces;
+using Application.ResponseModel;
+using AutoMapper;
+using Domain.Entities;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.Filters;
 
 namespace WebUI.Controllers
 {
@@ -7,5 +16,73 @@ namespace WebUI.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
+        private readonly IBookService _bookService;
+        private readonly IMapper _mapper;
+        private readonly IValidator<Book> _validator;
+
+        public AuthorController(IBookService bookService, IMapper mapper, IValidator<Book> validator)
+        {
+            _bookService = bookService;
+            _mapper = mapper;
+            _validator = validator;
+        }
+        [HttpPost]
+        [Route("[action]")]
+        [ModelValidation]
+        public async Task<ActionResult<ResponseCore<List<BookCreateDTO>>>> CreateBook([FromBody] BookCreateDTO bookDTO)
+        {
+            Book book = _mapper.Map<Book>(bookDTO);
+            var validationResult = _validator.Validate(book);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(new ResponseCore<UserCreateDTO>(false, validationResult.Errors));
+            }
+            await _bookService.AddAsync(book);
+            var result = _mapper.Map<BookCreateDTO>(book);
+            ResponseCore<BookCreateDTO> responseCore = new ResponseCore<BookCreateDTO>()
+            {
+                IsSuccess = true,
+                Result = result
+            };
+            return Ok(responseCore);
+        }
+
+
+        [HttpPut]
+        [Route("[action]")]
+        [ModelValidation]
+        public async Task<ActionResult<ResponseCore<List<BookUpdateDTO>>>> UpdateBook([FromBody] BookUpdateDTO bookDTO)
+        {
+            Book book = _mapper.Map<Book>(bookDTO);
+            var validationResult = _validator.Validate(book);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(new ResponseCore<UserUpdateDTO>(false, validationResult.Errors));
+            }
+            await _bookService.UpdateAsync(book);
+            var result = _mapper.Map<BookUpdateDTO>(book);
+            ResponseCore<BookUpdateDTO> responseCore = new ResponseCore<BookUpdateDTO>()
+            {
+                IsSuccess = true,
+                Result = result
+            };
+            return Ok(responseCore);
+        }
+
+
+
+        [HttpPut]
+        [Route("[action]")]
+        [ModelValidation]
+        public async Task<ActionResult<ResponseCore<bool>>> DeleteBook(Guid Id)
+        {
+            Book book = await _bookService.Get(Id);
+            if (book == null)
+            {
+                return new ResponseCore<bool>() { Result = false };
+            }
+            await _bookService.DeleteAsync(book);
+            return new ResponseCore<bool>() { Result = true };
+        }
     }
 }
